68edac9b8614dd83c1fe0780967cd0a787898297
Your Name <joosep.pata@gmail.com>
2019-03-26 20:07:28 -0700

    process all

---
+++ analysiscpu.py
@@ -0,0 +1,246 @@
+#need to set these explicitly
+import os
+import numba
+import numpy as np
+import math
+
+@numba.jit
+def searchsorted(arr, val):
+    ret = -1
+    for i in range(len(arr)):
+        if val <= arr[i]:
+            ret = i
+            break
+    return ret
+
+#need atomics to add to bin contents
+@numba.jit
+def fill_histogram(data, weights, bins, out_w, out_w2):
+    for i in range(len(data)):
+        bin_idx = searchsorted(bins, data[i])
+        if bin_idx >=0 and bin_idx < len(out_w):
+            out_w[bin_idx] += weights[i]
+            out_w2[bin_idx] += weights[i]**2
+
+@numba.njit(parallel=True)
+def select_opposite_sign_muons_kernel(muon_charges_content, muon_charges_offsets, content_mask_in, content_mask_out):
+    
+    for iev in numba.prange(muon_charges_offsets.shape[0]-1):
+        start = muon_charges_offsets[iev]
+        end = muon_charges_offsets[iev + 1]
+        
+        ch1 = 0
+        idx1 = -1
+        ch2 = 0
+        idx2 = -1
+        
+        for imuon in range(start, end):
+            if not content_mask_in[imuon]:
+                continue
+                
+            if idx1 == -1:
+                ch1 = muon_charges_content[imuon]
+                idx1 = imuon
+                continue
+            else:
+                ch2 = muon_charges_content[imuon]
+                if (ch2 != ch1):
+                    idx2 = imuon
+                    content_mask_out[idx1] = 1
+                    content_mask_out[idx2] = 1
+                    break
+    return
+
+@numba.njit(parallel=True)
+def sum_in_offsets_kernel(content, offsets, mask_rows, mask_content, out):
+
+    for iev in numba.prange(offsets.shape[0]-1):
+        if not mask_rows[iev]:
+            continue
+            
+        start = offsets[iev]
+        end = offsets[iev + 1]
+        for ielem in range(start, end):
+            if mask_content[ielem]:
+                out[iev] += content[ielem]
+            
+@numba.njit(parallel=True)
+def max_in_offsets_kernel(content, offsets, mask_rows, mask_content, out):
+
+    for iev in numba.prange(offsets.shape[0]-1):
+        if not mask_rows[iev]:
+            continue
+            
+        start = offsets[iev]
+        end = offsets[iev + 1]
+    
+        first = True
+        accum = 0
+        
+        for ielem in range(start, end):
+            if mask_content[ielem]:
+                if first or content[ielem] > accum:
+                    accum = content[ielem]
+                    first = False
+        out[iev] = accum
+
+        
+@numba.njit(parallel=True)
+def min_in_offsets_kernel(content, offsets, mask_rows, mask_content, out):
+
+    for iev in numba.prange(offsets.shape[0]-1):
+        if not mask_rows[iev]:
+            continue
+            
+        start = offsets[iev]
+        end = offsets[iev + 1]
+    
+        first = True
+        accum = 0
+        
+        for ielem in range(start, end):
+            if mask_content[ielem]:
+                if first or content[ielem] < accum:
+                    accum = content[ielem]
+                    first = False
+        out[iev] = accum
+    
+@numba.njit(parallel=True)
+def get_in_offsets_kernel(content, offsets, indices, mask_rows, mask_content, out):
+    for iev in numba.prange(offsets.shape[0]-1):
+        if not mask_rows[iev]:
+            continue
+        start = offsets[iev]
+        end = offsets[iev + 1]
+        
+        index_to_get = 0
+        for ielem in range(start, end):
+            if mask_content[ielem]:
+                if index_to_get == indices[iev]:
+                    out[iev] = content[ielem]
+                    break
+                else:
+                    index_to_get += 1
+        
+@numba.njit(parallel=True)
+def min_in_offsets_kernel(content, offsets, mask_rows, mask_content, out):
+    for iev in numba.prange(offsets.shape[0]-1):
+        if not mask_rows[iev]:
+            continue
+            
+        start = offsets[iev]
+        end = offsets[iev + 1]
+    
+        first = True
+        accum = 0
+        
+        for ielem in range(start, end):
+            if mask_content[ielem]:
+                if first or content[ielem] < accum:
+                    accum = content[ielem]
+                    first = False
+        out[iev] = accum
+        
+def sum_in_offsets(struct, content, mask_rows, mask_content, dtype=None):
+    if not dtype:
+        dtype = content.dtype
+    sum_offsets = np.zeros(len(struct.offsets) - 1, dtype=dtype)
+    sum_in_offsets_kernel(content, struct.offsets, mask_rows, mask_content, sum_offsets)
+    return sum_offsets
+
+def max_in_offsets(struct, content, mask_rows, mask_content):
+    max_offsets = np.zeros(len(struct.offsets) - 1, dtype=content.dtype)
+    max_in_offsets_kernel(content, struct.offsets, mask_rows, mask_content, max_offsets)
+    return max_offsets
+
+def min_in_offsets(struct, content, mask_rows, mask_content):
+    max_offsets = np.zeros(len(struct.offsets) - 1, dtype=content.dtype)
+    min_in_offsets_kernel(content, struct.offsets, mask_rows, mask_content, max_offsets)
+    return max_offsets
+
+def select_muons_opposite_sign(muons, in_mask):
+    out_mask = np.invert(muons.make_mask())
+    select_opposite_sign_muons_kernel(muons.charge, muons.offsets, in_mask, out_mask)
+    return out_mask
+
+def get_in_offsets(content, offsets, indices, mask_rows, mask_content):
+    out = np.zeros(len(offsets) - 1, dtype=content.dtype)
+    get_in_offsets_kernel(content, offsets, indices, mask_rows, mask_content, out)
+    return out
+
+"""
+For all events (N), mask the objects in the first collection (M1) if they are closer than dr2 to any object in the second collection (M2).
+
+    etas1: etas of the first object, array of (M1, )
+    phis1: phis of the first object, array of (M1, )
+    mask1: mask (enabled) of the first object, array of (M1, )
+    offsets1: offsets of the first object, array of (N, )
+
+    etas2: etas of the second object, array of (M2, )
+    phis2: phis of the second object, array of (M2, )
+    mask2: mask (enabled) of the second object, array of (M2, )
+    offsets2: offsets of the second object, array of (N, )
+    
+    mask_out: output mask, array of (M1, )
+
+"""
+@numba.jit
+def mask_deltar_first_kernel(etas1, phis1, mask1, offsets1, etas2, phis2, mask2, offsets2, dr2, mask_out):
+    
+    for iev in range(len(offsets1)-1):
+        a1 = offsets2[iev]
+        b1 = offsets2[iev+1]
+        
+        a2 = offsets2[iev]
+        b2 = offsets2[iev+1]
+        
+        for idx1 in range(a1, b1):
+            if not mask1[idx1]:
+                continue
+                
+            eta1 = etas1[idx1]
+            phi1 = phis1[idx1]
+            for idx2 in range(a2, b2):
+                if not mask2[idx2]:
+                    continue
+                eta2 = etas2[idx2]
+                phi2 = phis2[idx2]
+                
+                deta = abs(eta1 - eta2)
+                dphi = (phi1 - phi2 + math.pi) % (2*math.pi) - math.pi
+                
+                #if first object is closer than dr2, mask element will be *disabled*
+                passdr = ((deta**2 + dphi**2) < dr2)
+                mask_out[idx1] = not passdr
+                
+def mask_deltar_first(objs1, mask1, objs2, mask2, drcut):
+    assert(mask1.shape == objs1.eta.shape)
+    assert(mask2.shape == objs2.eta.shape)
+    assert(objs1.offsets.shape == objs2.offsets.shape)
+    
+    mask_out = np.ones_like(objs1.eta, dtype=np.bool)
+    mask_deltar_first_kernel(
+        objs1.eta, objs1.phi, mask1, objs1.offsets,
+        objs2.eta, objs2.phi, mask2, objs2.offsets,
+        drcut**2, mask_out
+    )
+    return mask_out
+
+def histogram_from_vector(data, weights, bins):        
+    out_w = np.zeros(len(bins) - 1, dtype=np.float32)
+    out_w2 = np.zeros(len(bins) - 1, dtype=np.float32)
+    fill_histogram(data, weights, bins, out_w, out_w2)
+    return out_w, out_w2, bins
+    
+@numba.njit(parallel=True)
+def get_bin_contents_kernel(values, edges, contents, out):
+    for i in numba.prange(len(values)):
+        v = values[i]
+        ibin = searchsorted(edges, v)
+        if ibin>=0 and ibin < len(contents):
+            out[i] = contents[ibin]
+
+def get_bin_contents(values, edges, contents, out):
+    assert(values.shape == out.shape)
+    assert(edges.shape[0] == contents.shape[0]+1)
+    get_bin_contents_kernel(values, edges, contents, out)
--- test.py
+++ test.py
@@ -1,10 +1,16 @@
-import hepaccelerate
-from hepaccelerate import Results
 import glob
 import time
 import numpy
 import os
+import sys
+import cupy
 
+import uproot
+import hepaccelerate
+from hepaccelerate import Results
+
+
+#Choose the backend
 use_cuda = bool(int(os.environ.get("HEPACCELERATE_CUDA", 0)))
 if use_cuda:
     print("Using the GPU CUDA backend")
@@ -19,25 +25,35 @@ else:
 def get_histogram(data, weights, bins):
     return hepaccelerate.Histogram(*histogram_from_vector(data, weights, bins))
 
-def get_selected_muons(muons, mu_pt_cut_leading, mu_pt_cut_subleading, mu_iso_cut):
-    
+def get_selected_muons(muons, mask_events, mu_pt_cut_leading, mu_pt_cut_subleading, mu_iso_cut):
+    """
+    Given a list of muons in events, selects the muons that pass quality, momentum and charge criteria.
+    Selects events that have at least 2 such muons. Selections are made by producing boolean masks.
+ 
+    muons (list of JaggedArray) - The muon content of a given file
+    mask_events (array of bool) - a mask of events that are used for muon processing
+    mu_pt_cut_leading (float) - the pt cut on the leading muon
+    mu_pt_cut_subleading (float) - the pt cut on all other muons
+    mu_iso_cut (float) - cut to choose isolated muons
+
+    """ 
     passes_iso = muons.pfRelIso03_all < mu_iso_cut
     passes_id = muons.mediumId == 1
     passes_subleading_pt = muons.pt > mu_pt_cut_subleading
     passes_leading_pt = muons.pt > mu_pt_cut_leading
     
-    #select events with at least 2 muons passing cuts
+    #select muons that pass these cuts
     muons_passing_id = passes_iso & passes_id & passes_subleading_pt
-    
-    events_all = NUMPY_LIB.ones(len(muons), dtype=NUMPY_LIB.bool)
-    events_passes_muid = sum_in_offsets(muons, muons_passing_id, events_all, muons.masks["all"], NUMPY_LIB.int8) >= 2
-    events_passes_leading_pt = sum_in_offsets(muons, muons_passing_id & passes_leading_pt, events_all, muons.masks["all"], NUMPY_LIB.int8) >= 1
-    events_passes_subleading_pt = sum_in_offsets(muons, muons_passing_id & passes_subleading_pt, events_all, muons.masks["all"], NUMPY_LIB.int8) >= 2
+   
+    #select events that have muons passing cuts 
+    events_passes_muid = sum_in_offsets(muons, muons_passing_id, mask_events, muons.masks["all"], NUMPY_LIB.int8) >= 2
+    events_passes_leading_pt = sum_in_offsets(muons, muons_passing_id & passes_leading_pt, mask_events, muons.masks["all"], NUMPY_LIB.int8) >= 1
+    events_passes_subleading_pt = sum_in_offsets(muons, muons_passing_id & passes_subleading_pt, mask_events, muons.masks["all"], NUMPY_LIB.int8) >= 2
 
-    base_event_sel = events_passes_muid & events_passes_leading_pt & events_passes_subleading_pt
+    base_event_sel = mask_events & events_passes_muid & events_passes_leading_pt & events_passes_subleading_pt
     
     muons_passing_os = select_muons_opposite_sign(muons, muons_passing_id & passes_subleading_pt)
-    events_passes_os = sum_in_offsets(muons, muons_passing_os, events_all, muons.masks["all"], NUMPY_LIB.int8) == 2
+    events_passes_os = sum_in_offsets(muons, muons_passing_os, mask_events, muons.masks["all"], NUMPY_LIB.int8) == 2
     
     final_event_sel = base_event_sel & events_passes_os
     final_muon_sel = muons_passing_id & passes_subleading_pt & muons_passing_os
@@ -48,14 +64,17 @@ def get_selected_muons(muons, mu_pt_cut_leading, mu_pt_cut_subleading, mu_iso_cu
     }
 
 
-def get_selected_jets(jets, muons, mask_muons, jet_pt_cut, jet_eta_cut, dr_cut):
-    events_all = NUMPY_LIB.ones(len(jets), dtype=NUMPY_LIB.bool)
-
+def get_selected_jets(jets, muons, mask_muons, mask_events, jet_pt_cut, jet_eta_cut, dr_cut):
+    """
+    Given jets and selected muons in events, choose jets that pass quality criteria and that are not dR-matched
+    to muons.
+ 
+    """
     jets_pass_dr = mask_deltar_first(jets, jets.masks["all"], muons, mask_muons, dr_cut)
     jets.masks["pass_dr"] = jets_pass_dr
     selected_jets = (jets.pt > jet_pt_cut) & (NUMPY_LIB.abs(jets.eta) < jet_eta_cut) & (((jets.jetId & 2)>>1)==1) & jets_pass_dr
 
-    num_jets = sum_in_offsets(jets, selected_jets, events_all, jets.masks["all"], NUMPY_LIB.int8)
+    num_jets = sum_in_offsets(jets, selected_jets, mask_events, jets.masks["all"], NUMPY_LIB.int8)
 
     return {
         "selected_jets": selected_jets,
@@ -87,22 +106,77 @@ def fill_with_weights(values, weights, weight_dict, mask, bins):
     }
     return ret
 
+def remove_inf_nan(arr):
+    arr[np.isinf(arr)] = 0
+    arr[np.isnan(arr)] = 0
+
+def compute_pu_weights(pu_corrections_target, weights, mc_nvtx, reco_nvtx):
+    pu_edges, (values_nom, values_up, values_down) = pu_corrections_target
+
+    src_pu_hist = get_histogram(mc_nvtx, weights, pu_edges)
+    norm = sum(src_pu_hist.contents)
+    src_pu_hist.contents = src_pu_hist.contents/norm
+    src_pu_hist.contents_w2 = src_pu_hist.contents_w2/norm
+
+
+    ratio = values_nom / src_pu_hist.contents
+    remove_inf_nan(ratio)
+    pu_weights = NUMPY_LIB.zeros_like(weights)
+    get_bin_contents(reco_nvtx, NUMPY_LIB.array(pu_edges), NUMPY_LIB.array(ratio), pu_weights)
+    
+    ratio_up = values_up / src_pu_hist.contents
+    remove_inf_nan(ratio_up)
+    pu_weights_up = NUMPY_LIB.zeros_like(weights)
+    get_bin_contents(reco_nvtx, NUMPY_LIB.array(pu_edges), NUMPY_LIB.array(ratio_up), pu_weights_up)
+    
+    ratio_down = values_down / src_pu_hist.contents
+    remove_inf_nan(ratio_down)
+    pu_weights_down = NUMPY_LIB.zeros_like(weights)
+    get_bin_contents(reco_nvtx, NUMPY_LIB.array(pu_edges), NUMPY_LIB.array(ratio_down), pu_weights_down)
+    
+    return pu_weights, pu_weights_up, pu_weights_down
+
+def select_events_trigger(scalars, mask_events):
+    mask_events = mask_events & scalars["HLT_IsoMu24"] & scalars["Flag_METFilters"] & scalars["Flag_goodVertices"]
+
 def analyze_data(
     muons, jets, scalars,
+    is_mc=True,
+    pu_corrections_target=None,
     mu_pt_cut_leading=26, mu_pt_cut_subleading=10,
     mu_iso_cut=0.3, jet_pt_cut=30,
-    jet_eta_cut=4.1, jet_mu_drcut=0.2, doverify=True
+    jet_eta_cut=4.7, jet_mu_drcut=0.4,
+    doverify=True,
+    debug=True
     ):
+    
+    weights = NUMPY_LIB.ones(len(muons), dtype=NUMPY_LIB.float32)
+    weights = weights# * scalars["genWeight"]
+    if debug:
+        print("Starting analysis with {0} events".format(len(weights)))
+
+    mask_events = NUMPY_LIB.ones(len(muons), dtype=NUMPY_LIB.bool)
+    select_events_trigger(scalars, mask_events)
+    if debug:
+        print("{0} events passed trigger".format(NUMPY_LIB.sum(mask_events)))
+
+    if is_mc:
+        pu_weights, pu_weights_up, pu_weights_down = compute_pu_weights(pu_corrections_target, weights, scalars["Pileup_nTrueInt"], scalars["PV_npvsGood"])
+        scalars["puWeight"] = pu_weights
+        scalars["puWeight_up"] = pu_weights_up
+        scalars["puWeight_down"] = pu_weights_down
 
     #get the two leading muons after applying all muon selection
-    ret_mu = get_selected_muons(muons, mu_pt_cut_leading, mu_pt_cut_subleading, mu_iso_cut)
+    ret_mu = get_selected_muons(muons, mask_events, mu_pt_cut_leading, mu_pt_cut_subleading, mu_iso_cut)
     
     if doverify:
         z = sum_in_offsets(muons, ret_mu["selected_muons"], ret_mu["selected_events"], ret_mu["selected_muons"], dtype=NUMPY_LIB.int8)
         assert(NUMPY_LIB.all(z[z!=0] == 2))
+    if debug:
+        print("{0} events passed muon".format(NUMPY_LIB.sum(ret_mu["selected_events"])))
 
     #get the passing jets for events that pass muon selection
-    ret_jet = get_selected_jets(jets, muons, ret_mu["selected_muons"], jet_pt_cut, jet_eta_cut, jet_mu_drcut)    
+    ret_jet = get_selected_jets(jets, muons, ret_mu["selected_muons"], mask_events, jet_pt_cut, jet_eta_cut, jet_mu_drcut)    
     if doverify:
         z = min_in_offsets(jets, jets.pt, ret_mu["selected_events"], ret_jet["selected_jets"])
         assert(NUMPY_LIB.all(z[z>0] > jet_pt_cut))
@@ -127,8 +201,6 @@ def analyze_data(
         assert(NUMPY_LIB.all(subleading_muon_pt[subleading_muon_pt>0] > mu_pt_cut_subleading))
 
     
-    weights = NUMPY_LIB.ones(len(muons), dtype=NUMPY_LIB.float32)
-    weights = weights * scalars["genWeight"]
     hist_inv_mass_d = fill_with_weights(inv_mass, weights, scalars, ret_mu["selected_events"], NUMPY_LIB.linspace(0,500,201))
 
     #get histograms of leading and subleading muon momenta
@@ -148,42 +220,84 @@ def analyze_data(
     return Results({
         "hist_inv_mass_d": Results(hist_inv_mass_d),
         "hist_mu0_pt": Results(hist_leading_muon_pt_d),
+        
         "hist_mu0_pt_weights": Results(hist_leading_muon_pt_d_weights),
         "hist_mu1_pt": Results(hist_subleading_muon_pt_d),
         
         "hist_mu0_eta": Results(hist_leading_muon_eta_d),
         "hist_mu1_eta": Results(hist_subleading_muon_eta_d),
         
-#         #"hist_dimuon_mass_gen": hist_inv_mass_gen_d,        
-#         "hist_dimuon_mass": hist_inv_mass_d,        
-        
         "hist_leading_jet_pt": Results(hist_leading_jet_pt_d),
         "hist_subleading_jet_pt": Results(hist_subleading_jet_pt_d),        
     })
 
+def load_puhist_target(filename):
+    fi = uproot.open("RunII_2016_data.root")
+    
+    h = fi["pileup"]
+    edges = np.array(h.edges)
+    values_nominal = np.array(h.values)
+    values_nominal = values_nominal / np.sum(values_nominal)
+    
+    h = fi["pileup_plus"]
+    values_up = np.array(h.values)
+    values_up = values_up / np.sum(values_up)
+    
+    h = fi["pileup_minus"]
+    values_down = np.array(h.values)
+    values_down = values_down / np.sum(values_down)
+    return edges, (values_nominal, values_up, values_down)
+
+def chunks(l, n):
+    """Yield successive n-sized chunks from l."""
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
 if __name__ == "__main__":
-    filenames = glob.glob("/nvmedata/store/mc/RunIIFall17NanoAOD/DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8/NANOAODSIM/**/*.root", recursive=True)[:40]
-    
-    arrays_ev = [
-        "PV_npvsGood", "Flag_METFilters", "Flag_goodVertices", "Generator_weight", "genWeight", "HLT_IsoMu24"
-    ]
-    arrays_jet = [
-        "Jet_pt", "Jet_eta", "Jet_phi", "Jet_btagDeepB", "Jet_jetId"
-    ]
-    
-    arrays_muon = [
-        "nMuon", "Muon_pt", "Muon_eta", "Muon_phi", "Muon_mass", "Muon_pfRelIso03_all", "Muon_mediumId", "Muon_charge"
-    ]
-    
-    arrays_to_load = arrays_ev + arrays_jet + arrays_muon
-    ds = hepaccelerate.NanoAODDataset(filenames, arrays_to_load, "Events", NUMPY_LIB)
-    #ds.preload(nthreads=16, do_progress=True)
-    #ds.to_cache(do_progress=True)
-    ds.from_cache(do_progress=True)
+    #
 
-    ds.make_random_weights()
 
-    ret_30 = ds.analyze(analyze_data, mu_pt_cut_leading=30)
-    ret_35 = ds.analyze(analyze_data, mu_pt_cut_leading=35)
-    ret_40 = ds.analyze(analyze_data, mu_pt_cut_leading=40)
-    ret_30.save_json("test.json")
+    for datasetname, globpattern, is_mc in [
+        ("data_2017", "/nvmedata/store/data/Run2017*/SingleMuon/NANOAOD/Nano14Dec2018-v1/**/*.root", False),
+        ("dy", "/nvmedata/store/mc/RunIIFall17NanoAOD/DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8/NANOAODSIM/**/*.root", True),
+        ("ggh", "/nvmedata/store/mc/RunIIFall17NanoAOD/GluGluHToMuMu_M125_13TeV_amcatnloFXFX_pythia8/NANOAODSIM/**/*.root", True),
+        ]:
+        filenames_all = glob.glob(globpattern, recursive=True)
+        filenames_all = [fn for fn in filenames_all if not "Friend" in fn]
+        for filenames in chunks(filenames_all, 30):
+            arrays_ev = [
+                "PV_npvsGood", "Flag_METFilters", "Flag_goodVertices", "HLT_IsoMu24"
+            ]
+            if is_mc:
+                arrays_ev += ["Pileup_nTrueInt", "Generator_weight", "genWeight"]
+            arrays_jet = [
+                "Jet_pt", "Jet_eta", "Jet_phi", "Jet_btagDeepB", "Jet_jetId"
+            ]
+            
+            arrays_muon = [
+                "nMuon", "Muon_pt", "Muon_eta", "Muon_phi", "Muon_mass", "Muon_pfRelIso03_all", "Muon_mediumId", "Muon_charge"
+            ]
+            
+            pu_corrections_2016 = load_puhist_target("RunII_2016_data.root") 
+ 
+            arrays_to_load = arrays_ev + arrays_jet + arrays_muon
+            ds = hepaccelerate.NanoAODDataset(filenames, arrays_to_load, "Events", NUMPY_LIB)
+            prepare_cache = "--prepare-cache" in sys.argv
+            
+            if prepare_cache:
+                ds.preload(nthreads=16, do_progress=True, event_vars=[bytes(x, encoding='ascii') for x in arrays_ev])
+                ds.to_cache(do_progress=True)
+            else:
+                ds.from_cache(do_progress=True)
+
+            #ds.make_random_weights()
+
+            ret = ds.analyze(analyze_data, is_mc=is_mc, pu_corrections_target=pu_corrections_2016, mu_pt_cut_leading=30, debug=False)
+            #ret_35 = ds.analyze(analyze_data, mu_pt_cut_leading=35)
+            #ret_40 = ds.analyze(analyze_data, mu_pt_cut_leading=40)
+            ret.save_json("{0}.json".format(datasetname))
+             
+            mempool = cupy.get_default_memory_pool()
+            pinned_mempool = cupy.get_default_pinned_memory_pool()
+            mempool.free_all_blocks()
+            pinned_mempool.free_all_blocks()
