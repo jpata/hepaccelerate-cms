39f3e6a10f2f3891759f08337e1043e6d9f9dfc2
Joosep Pata <joosep.pata@gmail.com>
2019-07-15 14:47:40 -0700

    added run script

---
+++ tests/hmm/run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+#Set NTHREADS=24 to run on whole machine, =1 for debugging
+NTHREADS=1
+
+#Set to -1 to run on all files, 1 for debugging
+MAXFILES=1
+
+#Set to 1 to run analysis on GPU, 0 for debugging
+HEPACCELERATE_CUDA=0
+
+#This is where the intermediate analysis files will be saved and loaded from
+CACHE_PATH=/storage/user/$USER/hmm/cache2
+
+##Step 1: cache ROOT data (need to repeat only when list of files or branches changes)
+PYTHONPATH=coffea:hepaccelerate:. NUMBA_THREADING_LAYER=tbb NUMBA_ENABLE_AVX=1 NUMBA_NUM_THREADS=$NTHREADS OMP_NUM_THREADS=$NTHREADS HEPACCELERATE_CUDA=0 time singularity exec --nv -B /storage /bigdata/shared/Software/singularity/gpuservers/singularity/images/cupy.simg python3 tests/hmm/analysis_hmumu.py --action cache --maxfiles $MAXFILES --chunksize 1 --nthreads $NTHREADS --cache-location $CACHE_PATH --datapath /storage/user/jpata/
+
+##Step 2: call physics code
+PYTHONPATH=coffea:hepaccelerate:. NUMBA_THREADING_LAYER=tbb NUMBA_ENABLE_AVX=1 NUMBA_NUM_THREADS=$NTHREADS OMP_NUM_THREADS=$NTHREADS time singularity exec --nv -B /nvme1/ -B /storage /bigdata/shared/Software/singularity/gpuservers/singularity/images/cupy.simg python3 tests/hmm/analysis_hmumu.py --action analyze --maxfiles $MAXFILES --chunksize 10 --cache-location $CACHE_PATH --datapath /storage/user/jpata/ --pinned --async-data --nthreads $NTHREADS
+
+##Step3: make plots
+PYTHONPATH=coffea:hepaccelerate:. singularity exec --nv -B /storage /bigdata/shared/Software/singularity/gpuservers/singularity/images/cupy.simg python3 cmsutils/plotting.py
