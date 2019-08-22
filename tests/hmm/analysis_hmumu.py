7ac3c7e679a8efe76010e890359ca929b20da259
Irene Dutta <33270474+irenedutta23@users.noreply.github.com>
2019-08-22 14:55:12 -0700

    Update train files and binning for DNN/histo
    New model file trained with VBF+ggh, new DNN binning in h peak with 15 bins,  change histo binning from 41 to 11

--- tests/hmm/analysis_hmumu.py
+++ tests/hmm/analysis_hmumu.py
@@ -326,8 +326,8 @@ class AnalysisCorrections:
 
             #load DNN model
             import keras
-            self.dnn_model = keras.models.load_model("data/DNN27vars_sig_vbf_bkg_dyvbf_dy105To160_ewk105To160_split_60_40_190808.h5")
-            self.dnn_normfactors = np.load("data/DNN27vars_sig_vbf_bkg_dyvbf_dy105To160_ewk105To160_split_60_40_190808.npy")
+            self.dnn_model = keras.models.load_model("data/DNN27vars_sig_vbf_ggh_bkg_dyvbf_dy105To160_ewk105To160_split_60_40_mod10_190821.h5")
+            self.dnn_normfactors = np.load("data/DNN27vars_sig_vbf_ggh_bkg_dyvbf_dy105To160_ewk105To160_split_60_40_mod10_190821.npy")
 
             if args.use_cuda:
                 import cupy
@@ -498,14 +498,14 @@ def main(args, datasets):
     histo_bins = {
         "muon_pt": np.linspace(0, 200, 101, dtype=np.float32),
         "npvs": np.linspace(0,100,101, dtype=np.float32),
-        "dijet_inv_mass": np.linspace(0, 2000, 41, dtype=np.float32),
-        "inv_mass": np.linspace(70, 150, 41, dtype=np.float32),
+        "dijet_inv_mass": np.linspace(0, 2000, 11, dtype=np.float32),
+        "inv_mass": np.linspace(70, 150, 11, dtype=np.float32),
         "numjet": np.linspace(0, 10, 11, dtype=np.float32),
         "jet_pt": np.linspace(0, 300, 101, dtype=np.float32),
-        "jet_eta": np.linspace(-4.7, 4.7, 41, dtype=np.float32),
-        "pt_balance": np.linspace(0, 5, 41, dtype=np.float32),
+        "jet_eta": np.linspace(-4.7, 4.7, 11, dtype=np.float32),
+        "pt_balance": np.linspace(0, 5, 11, dtype=np.float32),
         "numjets": np.linspace(0, 10, 11, dtype=np.float32),
-        "jet_qgl": np.linspace(0, 1, 41, dtype=np.float32)
+        "jet_qgl": np.linspace(0, 1, 11, dtype=np.float32)
     }
     for hname, bins in analysis_parameters["baseline"]["dnn_input_histogram_bins"].items():
         histo_bins[hname] = np.linspace(bins[0], bins[1], bins[2], dtype=np.float32)
@@ -515,7 +515,7 @@ def main(args, datasets):
         histo_bins["inv_mass_{0}".format(masswindow)] = np.linspace(mw[0], mw[1], 41, dtype=np.float32)
 
     histo_bins["dnn_pred2"] = {
-        "h_peak": np.array([0., 0.9, 0.91, 0.92, 0.93, 0.94, 0.95, 0.96, 0.97, 0.98, 1.0], dtype=np.float32),
+        "h_peak": np.array([0., 0.905, 0.915, 0.925, 0.935, 0.94, 0.945, 0.95, 0.955, 0.96, 0.965,0.97, 0.975,0.98, 0.985,1.0], dtype=np.float32),
         "z_peak": np.array([0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 1.0], dtype=np.float32),
         "h_sideband": np.array([0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 1.0], dtype=np.float32),
     }
