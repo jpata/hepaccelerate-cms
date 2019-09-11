02020a240eaae3f18c5afcaeac904a3823af37d6
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2019-09-10 23:11:18 -0400

    Remove C++ implementation of e-b-e resolution

--- tests/hmm/MiscVariables.cc
+++ tests/hmm/MiscVariables.cc
@@ -27,35 +27,3 @@ std::pair<double,double> CSAngles(TLorentzVector& v1, TLorentzVector& v2, int ch
     }
     return std::pair<double,double>(-thetaCS,-phiCS);
 }
-
-//simple uncertainty propagation to get di-muon invariant mass uncertainty, given ptErr on the two muons
-double mllErr(TLorentzVector& lep1, TLorentzVector& lep2, double ptErr1, double ptErr2){
-// double mllErr(double muPt1,  double muEta1, double muPhi1, double muMass1,
-// 	      double muPt2,  double muEta2, double muPhi2, double muMass2,
-// 	      double ptErr1, double ptErr2){
-
-  // TLorentzVector lep1; 
-  // TLorentzVector lep2;
-
-  // lep1.SetPtEtaPhiM(muPt1, muEta1, muPhi1, muMass1);
-  // lep2.SetPtEtaPhiM(muPt2, muEta2, muPhi2, muMass2);
-
-  // just for simplicity
-  double pt1 = lep1.Pt();
-  double pt2 = lep2.Pt();
-
-  // event is OK if both muons have pt > 0
-  if (pt1 > 0. && pt2 > 0.){
-
-    double mass = (lep1+lep2).M();
-    double dpt1 = (ptErr1 * mass) / (2 * pt1);
-    double dpt2 = (ptErr2 * mass) / (2 * pt2);
-    double err  = sqrt(dpt1*dpt1 + dpt2*dpt2);
-
-    return err;
-  }
-  else {
-    return -9999.0;
-  }
-
-}
--- tests/hmm/MiscVariables.h
+++ tests/hmm/MiscVariables.h
@@ -3,41 +3,20 @@
 
 std::pair<double,double> CSAngles(TLorentzVector& v1, TLorentzVector& v2, int charge);
 
-double mllErr(TLorentzVector& lep1, TLorentzVector& lep2, double ptErr1, double ptErr2);
-
 extern "C" {
-  void csangles_eval(
-		     float* out_theta, float* out_phi, int nev,
-		     float* pt1, float* eta1, float* phi1, float* mass1,
-		     float* pt2, float* eta2, float* phi2, float* mass2,
-		     int* charges) {
-#pragma omp parallel for default(none) shared(out_theta, out_phi, nev, pt1, eta1, phi1, mass1, pt2, eta2, phi2, mass2, charges) schedule(dynamic, 1000)
-    for (int iev=0; iev<nev; iev++) {
+    void csangles_eval(
+        float* out_theta, float* out_phi, int nev,
+        float* pt1, float* eta1, float* phi1, float* mass1,
+        float* pt2, float* eta2, float* phi2, float* mass2,
+        int* charges) {
+            #pragma omp parallel for default(none) shared(out_theta, out_phi, nev, pt1, eta1, phi1, mass1, pt2, eta2, phi2, mass2, charges) schedule(dynamic, 1000)
+            for (int iev=0; iev<nev; iev++) {
                 TLorentzVector v1, v2;
                 v1.SetPtEtaPhiM(pt1[iev], eta1[iev], phi1[iev], mass1[iev]);
                 v2.SetPtEtaPhiM(pt2[iev], eta2[iev], phi2[iev], mass2[iev]);
                 const auto ret = CSAngles(v1, v2, charges[iev]);
                 out_theta[iev] = (float)(ret.first);
                 out_phi[iev] = (float)(ret.second);
+            }
     }
-  }
-  void mllErr_eval(
-		     float* out_Err, int nev,
-		     float* pt1, float* eta1, float* phi1, float* mass1,
-		     float* pt2, float* eta2, float* phi2, float* mass2,
-		     float* pt1Err, float* pt2Err) {
-#pragma omp parallel for default(none) shared(out_Err, nev, pt1, eta1, phi1, mass1, pt2, eta2, phi2, mass2, pt1Err, pt2Err) schedule(dynamic, 1000)
-    for (int iev=0; iev<nev; iev++) {
-                TLorentzVector v1, v2;
-                v1.SetPtEtaPhiM(pt1[iev], eta1[iev], phi1[iev], mass1[iev]);
-                v2.SetPtEtaPhiM(pt2[iev], eta2[iev], phi2[iev], mass2[iev]);
-                const auto ret = mllErr(v1, v2, pt1Err[iev], pt2Err[iev]);   
-                out_Err[iev] = (float)(ret);
-    }
-  }
 }
-
-//double mllErr(TLorentzVector& lep1, TLorentzVector& lep2, double ptErr1, double ptErr2);
-/* double mllErr(double muPt1,  double muEta1, double muPhi1, double muMass1, */
-/* 	      double muPt2,  double muEta2, double muPhi2, double muMass2, */
-/* 	      double ptErr1, double ptErr2); */
