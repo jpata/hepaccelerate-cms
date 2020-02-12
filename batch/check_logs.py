5e150f5e86d2e50ad2e6504e0ac6fd40c32f7293
Joosep Pata <joosep.pata@cern.ch>
2020-02-12 10:39:36 -0800

    comment log analysis script

--- batch/check_logs.py
+++ batch/check_logs.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# Analyzes the job logs for the hmm_analyze.sh step 
 import sys, glob
 import numpy as np
 
@@ -6,29 +8,42 @@ def parse_log(fn):
     spd = None
     maxrss = None
     time = None
+    evs = None
+    size_gb = None
     for line in lines:
         if "maxrss" in line:
             maxrss = float(line.split()[0].split("=")[1])
         elif "run_analysis" in line:
             spd = float(line.split()[15])
             time = float(line.split()[13])
-    return time, maxrss, spd
+            size_gb = float(line.split()[11])
+            evs = float(line.split()[3])
+    return time, maxrss, spd, evs, size_gb
 
 if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print('./check_logs.py "logs/example_job.out.JOB_ID.*"')
+
     logpattern = sys.argv[1]
     maxrss = []
     speeds = []
     times = []
+    num_events = 0
+    size_gb = 0
+
     for fn in glob.glob(logpattern):
-        time, m_rss, spd = parse_log(fn)
+        time, m_rss, spd, evs, _size_gb = parse_log(fn)
         if m_rss is None or spd is None:
             print("Could not parse log {0}".format(fn))
             continue 
         times += [time]
         maxrss += [m_rss]
         speeds += [spd]
+        num_events += evs
+        size_gb += _size_gb
 
     print("number of jobs: {0}".format(len(times))) 
     print("runtime: {0:.2f} +- {1:.2f} s".format(np.mean(times), np.std(times)))
     print("maxrss: {0:.2f} +- {1:.2f} MB".format(np.mean(maxrss), np.std(maxrss)))
     print("speeds: {0:.2f} +- {1:.2f} Hz".format(np.mean(speeds), np.std(speeds)))
+    print("total processed: {} events, {:.2f} GB".format(int(num_events), size_gb))
